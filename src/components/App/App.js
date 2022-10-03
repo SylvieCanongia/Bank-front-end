45413d99ea3dbb9b5351e533cfd25963b7438e5f
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-10-03 18:24:02 +0200

    :sparkles: feat: added Home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
+import Routes from '../Routes/Routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Routes />
     </div>
   );
 }
---
+++ src/views/Home/Home.js
@@ -0,0 +1,83 @@
+import { NavLink } from "react-router-dom";
+import ArgentBankLogo from './../../assets/img/argentBankLogo.png';
+import IconChat from './../../assets/img/icon-chat.png';
+import IconMoney from './../../assets/img/icon-money.png';
+import IconSecurity from './../../assets/img/icon-security.png';
+
+const Home = () => {
+  return (
+    
+    <div className="home">
+      <nav className="main-nav">
+        <NavLink exact="true" to="/">
+          <div className="main-nav-logo">
+            <img
+              className="main-nav-logo-image"
+              src={ ArgentBankLogo }
+              alt="Argent Bank Logo"
+            />
+            <h1 className="sr-only">Argent Bank</h1>
+          </div>
+        </NavLink>
+        
+        <NavLink exact="true" to="/signIn">
+          <div className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </div>
+        </NavLink>
+      </nav>
+      <main>
+        <div className="hero">
+          <section className="hero-content">
+            <h2 className="sr-only">Promoted Content</h2>
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+            <p className="text">Open a savings account with Argent Bank today!</p>
+          </section>
+        </div>
+        <section className="features">
+          <h2 className="sr-only">Features</h2>
+          <div className="feature-item">
+            <img src={ IconChat } alt="Chat Icon" className="feature-icon" />
+            <h3 className="feature-item-title">You are our #1 priority</h3>
+            <p>
+              Need to talk to a representative? You can get in touch through our
+              24/7 chat or through a phone call in less than 5 minutes.
+            </p>
+          </div>
+          <div className="feature-item">
+            <img
+              src={ IconMoney }
+              alt="Chat Icon"
+              className="feature-icon"
+            />
+            <h3 className="feature-item-title">More savings means higher rates</h3>
+            <p>
+              The more you save with us, the higher your interest rate will be!
+            </p>
+          </div>
+          <div className="feature-item">
+            <img
+              src={ IconSecurity }
+              alt="Chat Icon"
+              className="feature-icon"
+            />
+            <h3 className="feature-item-title">Security you can trust</h3>
+            <p>
+              We use top of the line encryption to make sure your data and money
+              is always safe.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </div>
+
+  );
+};
+
+export default Home;
