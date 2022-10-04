3e1dff3b3bdfc368d5e4aaadb9ddef6f2659a43a
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-10-04 10:11:41 +0200

    :recycle: refactor: added dynamic year to copyright in the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
+const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
-      <p className="footer-text">Copyright 2020 Argent Bank</p>
+      <p className="footer-text">Copyright { currentYear } Argent Bank</p>
     </footer>
   );
 };
