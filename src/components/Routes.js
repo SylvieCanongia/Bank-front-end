9f7825773a99bb772bde4ca2120c68e1be7242ce
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-10-04 10:46:37 +0200

    :sparkles: feat: added SignIn page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
 
 const index = () => {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
+      <Route path="/signin" element={ <SignIn /> } />
     </Routes>
   );
 };
---
+++ src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import SignInForm from '../components/SignInForm';
+
+const SignIn = () => {
+  return (
+    <main class="main bg-dark">
+      <SignInForm />
+    </main>
+  );
+};
+
+export default SignIn;
