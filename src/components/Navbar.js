9212e2613d020758a3fdf64e1de2a5f1c32aac4e
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2022-12-09 09:44:47 +0100

    :recycle: refacotr: added logout  + home link in user profile connected page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,28 @@ const Navbar = () => {
   return (
      <nav className="main-nav">
         <NavLink end="true" to="/" className="main-nav-logo">
-            <img
-              className="main-nav-logo-image"
-              src={ ArgentBankLogo }
-              alt="Argent Bank Logo"
-            />
-            <h1 className="sr-only">Argent Bank</h1>
+          <img
+            className="main-nav-logo-image"
+            src={ ArgentBankLogo }
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
         </NavLink>
         <div>
-        <NavLink className="main-nav-item" end="true" to="/login">
-              <i className="fa fa-user-circle"></i>
-              Sign In
-        </NavLink>
+          <NavLink className="main-nav-item" end="true" to="/login">
+                <i className="fa fa-user-circle"></i>
+                Sign In
+          </NavLink>
+        </div>
+        <div>
+          <NavLink end="true" to="/profile" className="main-nav-item">
+            <i class="fa fa-user-circle"></i>
+              Tony
+          </NavLink>
+          <NavLink end="true" to="/" className="main-nav-item">
+            <i class="fa fa-sign-out"></i>
+              Sign Out
+          </NavLink>
         </div>
       </nav>
   );
